df5e27bca20eafce5881aabc47e58b043d8cfb24
Zayden <zaylanthier-lang>
2026-05-06 14:14:06 -0400

    Finished Code

--- lib/song.py
+++ lib/song.py
@@ -1,18 +1,18 @@
 class Song:
-    # Class attributes shared by all Song objects
+    # Class attributes
     count = 0
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
-        # Instance attributes for each song
+        # Instance attributes
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        # Update class-level information when a new song is created
+        # Update class data whenever a new song is created
         Song.add_song_to_count()
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
@@ -21,24 +21,24 @@ class Song:
 
     @classmethod
     def add_song_to_count(cls):
-        # Increase total song count by 1
+        # Increase total song count
         cls.count += 1
 
     @classmethod
     def add_to_genres(cls, genre):
-        # Add genre only if it is not already in the list
+        # Add unique genres only
         if genre not in cls.genres:
             cls.genres.append(genre)
 
     @classmethod
     def add_to_artists(cls, artist):
-        # Add artist only if they are not already in the list
+        # Add unique artists only
         if artist not in cls.artists:
             cls.artists.append(artist)
 
     @classmethod
     def add_to_genre_count(cls, genre):
-        # Count how many songs exist for each genre
+        # Track number of songs per genre
         if genre not in cls.genre_count:
             cls.genre_count[genre] = 1
         else:
@@ -46,8 +46,8 @@ class Song:
 
     @classmethod
     def add_to_artists_count(cls, artist):
-        # Count how many songs exist for each artist
-        if artist not in cls.artists_count:
-            cls.artists_count[artist] = 1
+        # Track number of songs per artist
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
         else:
-            cls.artists_count[artist] += 1
+            cls.artist_count[artist] += 1
